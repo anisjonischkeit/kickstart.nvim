b63c781d47c4725b7ea5e1edd4e48be141cddcad
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2025-01-12 13:18:05 +1100

    plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+require 'custom.ft' -- filetypes definitions
 --[[
 
 =====================================================================
@@ -91,7 +92,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -599,11 +600,13 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        --tsserver = {},
         --
 
         jdtls = {},
-        ['mdx-analyzer'] = {},
+        ['mdx_analyzer'] = {
+          filetypes = { 'markdown', 'mdx' },
+        },
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
---
+++ lua/custom/plugins/showkeys.lua
@@ -0,0 +1,8 @@
+return {
+  'nvchad/showkeys',
+  cmd = 'ShowkeysToggle',
+  opts = {
+    show_count = true,
+    position = 'top-right',
+  },
+}
